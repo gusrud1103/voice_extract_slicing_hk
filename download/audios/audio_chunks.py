fba8d3ff7ac6da789cc15836b580f538313d13ee
gusrud1103 <35495333+gusrud1103@users.noreply.github.com>
2018-12-29 00:45:31 +0900

    Update audio_chunks.py
    change slicing method

--- download/audios/audio_chunks.py
+++ download/audios/audio_chunks.py
@@ -1,4 +1,4 @@
-﻿import os
+import os
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
 
@@ -9,28 +9,28 @@ def AudioChuncks(filePath, format):
     sound = AudioSegment.from_file("data/"+filePath, format=format)
     sound = sound.set_channels(1)
 
-    #chunks = split_on_silence(
-    #    sound,
-        # 0.5초(500ms)보다 긴 무음을 무음으로 간주.
-    #     min_silence_len = 500,
-        # -50 dBFS 미만의 소리는 없는것으로 간주.
-    #    silence_thresh = -50,
-    #)
+    chunks = split_on_silence(
+       sound,
+        #0.5초(500ms)보다 긴 무음을 무음으로 간주.
+        min_silence_len = 200,
+        #-50 dBFS 미만의 소리는 없는것으로 간주.
+       silence_thresh = -50,
+    )
 
-    #soundCount = 0
-    #for chunk in chunks:
-        # 잘려진 음성의 길이가 0.8초 이상일때 사용함.
-    #    if chunk.duration_seconds > 0.8:
-    #        chunk.export("chuncks/"+os.path.splitext(filePath)[0]+"{0}.wav".format(soundCount), format="wav")
-    #        soundCount += 1
-    
     soundCount = 0
-    for i, chunk in enumerate(sound[::10000]):
-      #with open("sound-%s.wav" % i, "wb") as f:
-        #chunk.export(f, format="wav")
-        if chunk.duration_seconds >= 10:
-            chunk.export("chuncks/"+os.path.splitext(filePath)[0]+"{0}.wav".format(soundCount), format="wav")
-            soundCount += 1
+    for chunk in chunks:
+        #잘려진 음성의 길이가 5초 이상일때 사용함.
+       if chunk.duration_seconds >= 3 and chunk.duration_seconds <=12:
+           chunk.export("chuncks/"+os.path.splitext(filePath)[0]+"{0}.wav".format(soundCount), format="wav")
+           soundCount += 1
+    
+    # soundCount = 0
+    # for i, chunk in enumerate(sound[::10000]):
+    #   #with open("sound-%s.wav" % i, "wb") as f:
+    #     #chunk.export(f, format="wav")
+    #     if chunk.duration_seconds >= 10:
+    #         chunk.export("chuncks/"+os.path.splitext(filePath)[0]+"{0}.wav".format(soundCount), format="wav")
+    #         soundCount += 1
 
     
 if __name__ == "__main__":
